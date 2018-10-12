082189000409a7cc308771daae7becc943c2e573
jie <1356573671@qq.com>
2018-10-12 20:39:41 +0800

    jie 完成登录界面

--- flybig/js/jie/jieJS.js
+++ flybig/js/jie/jieJS.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
+	
+//	切换登录状态
 	$("#choose_loginType label").click(function(){
 		var $label = $("#choose_loginType label");
 		for (var i=0;i< $label.length;i++){
 			$label.eq(i).removeClass("logintype_active");
 		}
 		$(this).addClass("logintype_active");
+	})
+	
+	$("#accountType").click(function(){
+		$("#account_login").removeClass("div_hidden");
+		$("#phone_login").addClass("div_hidden");
+	})
+	
+	$("#phoneType").click(function(){
+		$("#account_login").addClass("div_hidden");
+		$("#phone_login").removeClass("div_hidden");
+	})
+	
+	
+	//切换注册状态
+	$("#change_phone_reg").click(function(){
+		$("#chang_email_reg").removeClass("div_hidden");
+		$("#change_phone_reg").addClass("div_hidden");
+		$("#email_reg").removeClass("div_hidden");
+		$("#phone_reg").addClass("div_hidden");
+	})
+	
+	$("#chang_email_reg").click(function(){
+		$("#chang_email_reg").addClass("div_hidden");
+		$("#change_phone_reg").removeClass("div_hidden");
+		$("#phone_reg").removeClass("div_hidden");
+		$("#email_reg").addClass("div_hidden");
 	})
 })
