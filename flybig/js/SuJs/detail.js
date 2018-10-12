bf78ea472bf6dd8c81681e4ee9faeb2941d46459
SuAiLing <279872663@qq.com>
2018-10-12 21:46:06 +0800

    4

--- flybig/js/SuJs/detail.js
+++ flybig/js/SuJs/detail.js
@@ -1,5 +1,36 @@
 $(function(){
 	//日期选择器
-	$('.date_picker').date_input();
 	
+	
+	//数量
+	
+	$(".btn_up").click(function(){
+		$a=$(".count>input").val();
+//		alert($a);
+		$a++;
+		$(this).parent().children("input").val($a);
+	})
+	$(".btn_down").click(function(){
+		$a=$(".count>input").val();
+//		alert($a);
+		$a--;
+		if($a>0){
+			$(this).parent().children("input").val($a);			
+		}else{
+			return false;
+		}
+	})
+	
+	//套餐选择
+	$(".type").click(function(){
+		$(this).parent("p").children(".type").children("i").hide();
+		$(this).parent("p").children(".type").children("span").css("border-color","#e0e0e0");
+		$(this).children("i").show();
+		$(this).children("span").css("border-color","orangered");
+		
+		
+		
+		
+		
+	})
 })
