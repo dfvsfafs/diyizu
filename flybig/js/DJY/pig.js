e5dcf5a1918369e70230e620bfc863ce011ae9af
DaiJiayu123 <965203685@qq.com>
2018-10-16 10:32:23 +0800

    DJY两个页面完1

--- flybig/js/DJY/pig.js
+++ flybig/js/DJY/pig.js
@@ -969,4 +969,23 @@ $(document).ready(function(){
 		$(".bigg1leftpage2").show();
 		$(".bigg1leftpage1").hide();
 	})
+	
+//	头部
+//header_search_btn点击搜索框变化
+	$(".header_search_btn button").click(function(){
+		$(this).addClass("search_switch_selected").siblings().removeClass("search_switch_selected");
+	})
+	$(".header_search_btn button:nth-of-type(1)").click(function(){
+		$(this).parent().next().children().eq(0).attr("placeholder","目的地/酒店/景点/签证等");
+	})
+	$(".header_search_btn button:nth-of-type(2)").click(function(){
+		$(this).parent().next().children().eq(0).attr("placeholder","");
+	})
+	$(".header_search_inp input").focus(function(){
+		$(this).parent().next().show();
+	})
+	$(".header_search_inp input").blur(function(){
+		$(this).parent().next().hide();
+	})
+	
 })
