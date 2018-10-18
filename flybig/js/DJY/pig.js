747febb2e6228ec89ac46895d66b15833f4eb67b
jie <1356573671@qq.com>
2018-10-18 17:50:08 +0800

    jie

--- flybig/js/DJY/pig.js
+++ flybig/js/DJY/pig.js
@@ -1059,36 +1059,36 @@ $(document).ready(function(){
 	})
 	
 
-	var vm_2 = new Vue({
-			el:'#YHGL',
-			data:{
-				id:"",
-				username:'',
-				userpwd:'',
-				Email:'',
-				phone:'',
-				user:[
-				{id:1,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
-				{id:2,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
-				{id:3,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
-				{id:4,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
-				{id:5,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
-				]
-			},
-			methods:{
-				removeTask: function(task){ //删除任务
-	           //指向Vue实例中的tasks
-	           _tasks = this.user;
-	           //remove
-	           _tasks.forEach(function(item, index){
-	             if(item.id == task.id){
-	               _tasks.splice(index, 1);
-	             }
-	           })
-	        },
-	        add(){
-					this.user.push({id:this.id,username:this.username,userpwd:this.userpwd,Email:this.Email,phone:this.phone});
-				}
-	
-			}
-		})
+//	var vm_2 = new Vue({
+//			el:'#YHGL',
+//			data:{
+//				id:"",
+//				username:'',
+//				userpwd:'',
+//				Email:'',
+//				phone:'',
+//				user:[
+//				{id:1,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
+//				{id:2,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
+//				{id:3,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
+//				{id:4,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"},
+//				{id:5,username:"aaa",userpwd:"123",Email:"1356573672@qq.com",phone:"18207680077"}
+//				]
+//			},
+//			methods:{
+//				removeTask: function(task){ //删除任务
+//	           //指向Vue实例中的tasks
+//	           _tasks = this.user;
+//	           //remove
+//	           _tasks.forEach(function(item, index){
+//	             if(item.id == task.id){
+//	               _tasks.splice(index, 1);
+//	             }
+//	           })
+//	        },
+//	        add(){
+//					this.user.push({id:this.id,username:this.username,userpwd:this.userpwd,Email:this.Email,phone:this.phone});
+//				}
+//	
+//			}
+//		})
